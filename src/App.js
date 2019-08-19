00e304c2c7d3e40291feb620325dd8be34c32780
unknown <cvlopes88@gmail.com>
2019-08-19 17:23:46 -0400

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
+    <Route exact Path="/" component={Home} />
+    <Route Path="/about" component={About} />
+    <Route Path="/contact" component={Contact} />
   </div>
 );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+        <Link to="/">
+          < a href="">Home</a>
+          </Link>
         </div>
         <div>
+          <Link to="/About">
           <a href="">About</a>
+          </Link>
         </div>
         <div>
+          <Link to="/Contact">
           <a href="">Contact</a>
+          </Link>
         </div>
       </div>
     </div>
